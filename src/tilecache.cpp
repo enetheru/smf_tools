df9f1a3bb87073ae6263ba5e59421d64a1cf1761
Samuel Nicholas <nicholas.samuel@gmail.com>
2014-08-08 13:08:57 +0930

    minor formaatting

--- src/tilecache.cpp
+++ src/tilecache.cpp
@@ -1,3 +1,4 @@
+#include "config.h"
 #include "tilecache.h"
 
 #include "util.h"
--- src/tilecache.h
+++ src/tilecache.h
@@ -1,6 +1,5 @@
 #ifndef TILECACHE_H
 #define TILECACHE_H
-#include "config.h"
 
 #include <OpenImageIO/imagebuf.h>
 #include <vector>
@@ -8,6 +7,7 @@
 
 class TileCache
 {
+    // member data
     uint32_t nTiles;
     uint32_t tileSize;
     std::vector< uint32_t > map;
@@ -15,13 +15,16 @@ class TileCache
 public:
     bool verbose, quiet;
 
+    //constructors
     TileCache( bool v = false, bool q = false )
         : verbose(v), quiet(q)
     { };
 
+    // modifications
     void push_back( std::string );
     void setTileSize( uint32_t t ){ tileSize = t; };
 
+    // data access
     uint32_t getNTiles ( ){ return nTiles; };
     uint32_t getNFiles ( ){ return fileNames.size(); };
     uint32_t getTileSize( ){ return tileSize; };
