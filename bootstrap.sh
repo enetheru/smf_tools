77823eb126e613d517adf8464596a0fa91e7afb3
Samuel Nicholas <nicholas.samuel@gmail.com>
2014-12-17 22:55:51 +1030

    brought bootstrap closer to the travis-ci script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
 
-add-apt-repository -y ppa:irie/openimageio
-apt-get update
-apt-get dist-upgrade -y
-apt-get build-dep -y openimageio
-apt-get install libgif-dev libraw-dev
-apt-get install -y git cmake
+add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+add-apt-repository --yes ppa:boost-latest/ppa
+add-apt-repository --yes ppa:irie/openimageio
+apt-get update -qq
+apt-get build-dep --yes openimageio
 
-mkdir -p ~/git/OpenImageIO
-cd ~/git/OpenImageIO
-git clone https://github.com/OpenImageIO/oiio.git
-cd oiio
-for branch in `git branch -a | grep remotes | grep -v HEAD | grep -v master`; do
-    git branch --track ${branch##*/} $branch
-done
-git pull --all
-#git checkout RB-1.4
-make -e INSTALLDIR=/usr dist_dir=
+#FIXME force specific version of toolchain for consistency
+
+su vagrant
 
 mkdir -p ~/git/enetheru
 cd ~/git/enetheru
 git clone https://github.com/enetheru/MapConv.git
-mkdir MapConv-build
-cd MapConv-build
-cmake ../MapConv -DCMAKE_INSTALL_PREFIX:PATH=/usr
+
+mkdir -p ~/git/OpenImageIO
+cd ~/git/OpenImageIO
+git clone -b RB-1.4 https://github.com/OpenImageIO/oiio.git
+cd oiio
+git apply ~/git/enetheru/MapConv/oiio.patch
+sudo make USE_QT=0 USE_OPENGL=0 USE_PYTHON=0 USE_FIELD3D=0 USE_OPENJPEG=0 USE_GIF=0 USE_OCIO=0 USE_OPENSSL=0 USE_LIBRAW=0 NOTHREADS=0 OIIO_BUILD_TOOLS=0 OIIO_BUILD_TESTS=0 -e dist_dir= INSTALLDIR=/usr
+
+cd ~/git/enetheru/MapConv
+mkdir build
+cd build
+cmake ../ -DCMAKE_INSTALL_PREFIX:PATH=/usr
 make
-make install
+sudo make install
