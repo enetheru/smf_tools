7bd5126586f28a06ae7d4ecfea3d8043f4ce4f81
Samuel Nicholas <nicholas.samuel@gmail.com>
2015-01-27 10:24:20 +0000

    addded test for bencchmark

---
+++ tests/smt_create_split_1024x/test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+oiiotool --resample 1024x1024 ../data/image_1.png -o test.jpg
+COMMAND="smt_convert --smt --tilesize 32x32 ./test.jpg"
+echo COMMAND = $COMMAND
+eval $TIMER $VALGRIND $COMMAND
+smt_info output.smt
+
+exit $?
--- tests/template/test.sh
+++ tests/template/test.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-COMMAND="echo command"
+COMMAND="smt_convert -v --smt --tilesize 32x32 --imagesize 1024x1024 ../data/image_1.png"
+
+# == Pre Test Actions ==
+#nothing
+
+# == Test Action ==
 echo COMMAND = $COMMAND
 eval $TIMER $VALGRIND $COMMAND
 
+# == Post test actions ==
+#nothing
 exit $?
