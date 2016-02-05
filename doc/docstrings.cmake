a1e4c399b6cd3dcf2f1484f899d9dfcb6187333a
Samuel Nicholas <nicholas.samuel@gmail.com>
2016-02-05 01:41:03 +0000

    updated cmake condifuration

---
+++ doc/docstrings.cmake
@@ -0,0 +1,12 @@
+# NOTE: cant cache multi line strings in cmake, so just include this file
+# whenever you need these strings
+
+set( DOC_TITLE "smf_tools" )
+
+set( DOC_DESCRIPTION_SHORT
+"creation and extraction of smf and smt files" )
+
+set( DOC_DESCRIPTION
+"This package contains tools to manipulate smf and smt files used in maps" )
+
+set( DOC_AUTHORS "Samuel Nicholas <nicholas.samuel@gmail.com>" )
